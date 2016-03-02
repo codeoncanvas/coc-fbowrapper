50348151cb2e5ab388104f9029ec97b8c51f5a2d
Rene Christen <accounts@renechristen.net>
2016-03-02 13:18:57 +1100

    refactor

--- src/cocFboWrapper.h
+++ src/cocFboWrapper.h
@@ -3,84 +3,103 @@
 #include "cinder/gl/Fbo.h"
 #include "cinder/Camera.h"
 
-using namespace ci;
 
 namespace coc {
-    class FboWrapper {
 
-    public:
+class FboWrapper {
 
-        gl::FboRef  fbo;
-        Area        fboArea;
-        CameraOrtho fboCam;
-        bool        isInit;
+public:
 
+    FboWrapper();
 
-        FboWrapper() : isInit(false) {}
+    void setup( glm::vec2 _size, bool _isOrtho = true, bool _hasAlpha = false );
+    void setup( glm::vec2 _size, ci::gl::Fbo::Format _fmt, bool _isOrtho = true );
 
-        gl::FboRef getRef() { return fbo; }
+    void draw();
+    void draw( glm::vec2 pos );
+    void draw( ci::Rectf bounds );
 
-        void setup( Area _fboArea, bool _isOrtho = true, bool _hasAlpha = false )
-        {
-            fboArea = _fboArea;
-            fbo = gl::Fbo::create( fboArea.getWidth(), fboArea.getHeight(), _hasAlpha );
-            if (_isOrtho) fboCam.setOrtho( 0, fboArea.getWidth(), fboArea.getHeight(), 0, -1, 1 );
-            isInit = true;
-        }
+    void bind();
+    void unbind();
+    void reset();
 
-        void setup( Area _fboArea, gl::Fbo::Format _fmt, bool _isOrtho = true )
-        {
-            fboArea = _fboArea;
-            fbo = gl::Fbo::create( fboArea.getWidth(), fboArea.getHeight(), _fmt );
-            if (_isOrtho) fboCam.setOrtho( 0, fboArea.getWidth(), fboArea.getHeight(), 0, -1, 1 );
-            isInit = true;
-        }
+    ci::gl::FboRef getFboRef();
+    ci::gl::TextureRef getTextureRef();
+    bool getIsSetup();
 
-        void draw() {
-            if (fbo) gl::draw( fbo->getColorTexture());//, fboArea );
-        }
+private:
 
-        void draw( vec2 pos ) {
-            if (fbo)
-            {
-                gl::draw( fbo->getColorTexture(), Rectf( pos.x, pos.y, fboArea.getWidth(), fboArea.getHeight() ) );
-            }
-        }
-
-        void draw( Rectf bounds ) {
-            if (fbo)
-            {
-                gl::draw( fbo->getColorTexture(), bounds );
-            }
-        }
-
-        void reset() {
-            fbo.reset();
-            isInit = false;
-        }
-
-        gl::TextureRef getTextureRef() {
-            return fbo->getColorTexture();
-        }
-
-        void bind() {
-            fbo->bindFramebuffer();
-            gl::pushViewport();
-            gl::viewport( fbo->getSize() );
-            gl::pushMatrices();
-            gl::setMatricesWindowPersp( fbo->getSize() );
-            gl::setMatrices(fboCam);
-        }
-
-        void unbind() {
-            gl::popMatrices();
-            gl::popViewport();
-            fbo->unbindFramebuffer();
-        }
-
-        bool getInit() { return isInit; }
+    ci::gl::FboRef      fbo;
+    glm::vec2           size;
+    ci::CameraOrtho     fboCam;
+    bool                isSetup;
 
 
-    };//class FboWrapper
+};//class FboWrapper
+
+FboWrapper::FboWrapper() : isSetup(false) {}
+
+ci::gl::FboRef FboWrapper::getFboRef() { return fbo; }
+
+void FboWrapper::setup( glm::vec2 _size, bool _isOrtho, bool _hasAlpha )
+{
+    size = _size;
+    fbo = ci::gl::Fbo::create( size.x, size.y, _hasAlpha );
+    if (_isOrtho) fboCam.setOrtho( 0, size.x, size.y, 0, -1, 1 );
+    isSetup = true;
+}
+
+void FboWrapper::setup( glm::vec2 _size, ci::gl::Fbo::Format _fmt, bool _isOrtho )
+{
+    size = _size;
+    fbo = ci::gl::Fbo::create( size.x, size.y, _fmt );
+    if (_isOrtho) fboCam.setOrtho( 0, size.x, size.y, 0, -1, 1 );
+    isSetup = true;
+}
+
+void FboWrapper::draw() {
+    if (fbo) ci::gl::draw( fbo->getColorTexture());//, fboArea );
+}
+
+void FboWrapper::draw( glm::vec2 pos ) {
+    if (fbo)
+    {
+        ci::gl::draw( fbo->getColorTexture(), ci::Rectf( pos.x, pos.y, size.x, size.y ) );
+    }
+}
+
+void FboWrapper::draw( ci::Rectf bounds ) {
+    if (fbo)
+    {
+        ci::gl::draw( fbo->getColorTexture(), bounds );
+    }
+}
+
+void FboWrapper::reset() {
+    fbo.reset();
+    isSetup = false;
+}
+
+ci::gl::TextureRef FboWrapper::getTextureRef() {
+    return fbo->getColorTexture();
+}
+
+void FboWrapper::bind() {
+    fbo->bindFramebuffer();
+    ci::gl::pushViewport();
+    ci::gl::viewport( fbo->getSize() );
+    ci::gl::pushMatrices();
+    ci::gl::setMatricesWindowPersp( fbo->getSize() );
+    ci::gl::setMatrices(fboCam);
+}
+
+void FboWrapper::unbind() {
+    ci::gl::popMatrices();
+    ci::gl::popViewport();
+    fbo->unbindFramebuffer();
+}
+
+bool FboWrapper::getIsSetup() { return isSetup; }
+
 };//namespace coc
 
